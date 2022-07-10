a496cdddd39632612999f7a0acecfb13d0596f56
Austin Harcarik <abharcarik@gmail.com>
2022-07-09 19:45:28 -0700

    ready for testing

--- username_scraper.py
+++ username_scraper.py
@@ -1,7 +1,7 @@
+import math
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import bs4
 
 driver_path = "C:\Program Files (x86)\chromedriver"
 usernames_url = "https://www.beerxchange.com/users"
@@ -11,28 +11,23 @@ driver = webdriver.Chrome(driver_path)
 driver.get(usernames_url)
 
 total_users = driver.find_element(By.ID, "homeTitle").text
-print(total_users)
+total_users = int(''.join(c for c in total_users if c.isdigit()))
+n_iters = math.ceil(total_users / 10)
+remainder = total_users % 10
+max_range = 12 # 10 users per page, divs range from 2 to 11
 
-# usernames = []
+usernames = []
+count = 1
+while count <= n_iters:
+    if count == n_iters:
+        max_range = remainder
+    for user in range(2, max_range):
+        username = driver.find_element(By.XPATH, f'/html/body/div[2]/section/div/div/div[{user}]/div/div[1]/div/div/h4/a').text
+        usernames.append(username)
+    driver.find_element(By.XPATH, "/html/body/div[2]/section/div/div/div[12]/ul/li[3]/a").click()
+    count += 1
 
-# count = 1
-# while count <= 10:
-#     for user in range(2, 12):
-#         username = driver.find_element(By.XPATH, f'/html/body/div[2]/section/div/div/div[{user}]/div/div[1]/div/div/h4/a').text
-#         usernames.append(username)
-#     driver.find_element(By.XPATH, "/html/body/div[2]/section/div/div/div[12]/ul/li[3]/a").click()
-#     count += 1
+output = pd.DataFrame(usernames, columns = "username")
+output.to_csv('usernames.csv')
 
-# print(usernames)
-
-# username_1 = driver.find_element(By.XPATH, '/html/body/div[2]/section/div/div/div[2]/div/div[1]/div/div/h4/a').text
-# username_2 = driver.find_element(By.XPATH, '/html/body/div[2]/section/div/div/div[3]/div/div[1]/div/div/h4/a').text
-# print(username_1)
-# print(username_2)
-
-driver.find_element(By.XPATH, "/html/body/div[2]/section/div/div/div[12]/ul/li[3]/a").click()
-# driver.find_element(By.XPATH, "/html/body/div[2]/section/div/div/div[12]/ul/li[3]/a").click()
-
-# /html/body/div[2]/section/div/div/div[2]/div/div[1]/div/div/h4/a
-
-# /html/body/div[2]/section/div/div/div[11]/div/div[1]/div/div/h4/a
+driver.quit()
