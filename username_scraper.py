0e7c983cc62d58cde336f6c4a4291584de6c54b5
Austin Harcarik <abharcarik@gmail.com>
2022-07-11 18:08:23 -0700

    connected to postgres

--- username_scraper.py
+++ username_scraper.py
@@ -1,33 +1,55 @@
 import pandas as pd
-import numpy as np
 import bs4 as bs
 import urllib.request
 import ssl
 import argparse
-from datetime import datetime
+import time
+from sqlalchemy import create_engine
+from dotenv import load_dotenv, find_dotenv
+import os
 
 def main(params):
-    n_users = params.n_users
-
-    hour = datetime.now().hour
-
-    page_ranges = np.linspace(1, n_users / 10, 25, dtype = int)
+    load_dotenv(find_dotenv())
     
-    page_start = page_ranges[hour]
-    page_end = page_ranges[hour + 1] - 1
+    verbose = params.verbose
+    page_start = params.page_start
+    page_end = params.page_end
+    include_first_page = params.include_first_page
+    db = params.db
+    db_username = params.db_username
+    db_password = params.db_password
+    host = params.host
+    port = params.port
+    table = params.table
+
+    if db == 'null':
+        db = os.environ.get('DB')
+    if db_username == 'null':
+        db_username = os.environ.get('DB_USERNAME')
+    if db_password == 'null':
+        db_password = os.environ.get('DB_PASSWORD')
+    if host == 'null': 
+        host = os.environ.get('HOST')
+    if port == 'null': 
+        port = os.environ.get('PORT')
+    if table == 'null':
+        table = os.environ.get('TABLE')
+
+    engine = create_engine(f'postgresql://{db_username}:{db_password}@{host}:{port}/{db}')
+    engine.connect()
 
     base_url = 'https://www.beerxchange.com/users'
-
-    file_name = f'usernames/usernamesp{page_start}-p{page_end}.csv'
 
     ssl._create_default_https_context = ssl._create_unverified_context
 
     usernames = []
 
-    if hour == 0: 
+    if include_first_page: 
         source = urllib.request.urlopen(base_url)
         soup = bs.BeautifulSoup(source,'lxml')
         user_divs = soup.find_all("h4", {"class": "media-heading"})
+        if verbose:
+            print('scraping first page')
         for i in range(len(user_divs)): 
             user = user_divs[i].text
             user = user.lstrip()
@@ -35,7 +57,9 @@ def main(params):
             user = user.strip()
             usernames.append(user)
 
-    for page in range(page_start, page_end + 1): 
+    for page in range(page_start, page_end): 
+        if verbose:
+            print(f'scraping page {page}')
         url = base_url + f'?p={page}'
         source = urllib.request.urlopen(url)
         soup = bs.BeautifulSoup(source,'lxml')
@@ -46,15 +70,31 @@ def main(params):
             user = user.split(' ', 1)[0]
             user = user.strip()
             usernames.append(user)
+        time.sleep(1)
+    
+    delete_query = f'delete from {table} where username in {usernames}'
+    delete_query = delete_query.replace('[', '(')
+    delete_query = delete_query[::-1]
+    delete_query = delete_query.replace(']', ')')
+    delete_query = delete_query[::-1]
+    engine.execute(delete_query)
 
-    output = pd.DataFrame(usernames, columns=['user'])
-    output.to_csv(file_name, index = False)
+    output = pd.DataFrame(usernames, columns=['username'])
+    output.to_sql(name = table, con = engine, index = False, if_exists = "append")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'scrape untappd usernames from beerxchange.com')
 
     parser.add_argument('--verbose', help = 'verbose output', default = True, type = bool)
-    parser.add_argument('--n_users', help = 'total number of users', default = 19568, type = int)
+    parser.add_argument('--page_start', help = 'page to start on', default = 1, type = int)
+    parser.add_argument('--page_end', help = 'page to end on', default = 25, type = int)
+    parser.add_argument('--include_first_page', help = 'first page has no page number', default = False, type = bool)
+    parser.add_argument('--db', help = 'postgres database name', default = 'null', type = str)
+    parser.add_argument('--db_username', help = 'postgres database username', default = 'null', type = str)
+    parser.add_argument('--db_password', help = 'postgres database password', default = 'null', type = str)
+    parser.add_argument('--host', help = 'postgres db hostname', default = 'null', type = str)
+    parser.add_argument('--port', help = 'postgres db port', default = 'null', type = str)
+    parser.add_argument('--table', help = 'postgres table name', default = 'null', type = str)
 
     args = parser.parse_args()
 
